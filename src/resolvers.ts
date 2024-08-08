d8a61fb02090043ed62ddb2f8882c7d9596ae86a
Kolesnyk, Maxym (external - Project) <maxym.kolesnyk@sap.com>
2024-08-08 12:55:46 +0200

    Added codegen and context

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,7 +1,12 @@
-export const resolvers = {
-    Query: {
-        featuredListings: (_, __, { dataSources }) => {
-          return dataSources.listingAPI.getFeaturedListings();
-        },
-    }
+import { Resolvers } from './types'
+
+export const resolvers: Resolvers = {
+  Query: {
+    featuredListings: (_, __, {dataSources}) => {
+      return dataSources.listingAPI.getFeaturedListings();
+    },
+    listing: (_, { id }, { dataSources }) => {
+      return dataSources.listingAPI.getListing(id);
+    },
+  },
 };
